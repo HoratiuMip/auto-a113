d1b618b1e8a0c3675dbba9d3f3ec7fa5a5413489
mipsan <tudor.vatca1@gmail.com>
2026-01-25 12:04:37 +0200

    BAS

--- bas.cmake
+++ bas.cmake
@@ -1,7 +1,8 @@
+# Copy and configure the following in your cmake script before adding the auto-a113 subdirectory.
 # Due to thinking about her I completely forgot what BAS stands for two minutes after making the file.
 
-# Copy and configure the following in tour cmake script before adding the auto-a113 subdirectory.
-# Add -DA113_USE_BAS as an argument to your cmake command line to use this configuration file.
+# Location of auto-a113.
+set( A113_ROOT_DIR "" )
 
 # Build target plate.
 # - "uC" - target is a microcontroller device.
@@ -11,3 +12,6 @@ set( A113_TARGET_PLATE "" )
 # Target operating system. This is relevant and needs to be set only if A113_TARGET_PLATE was configured to "OS".
 # - "Windows"
 set( A113_TARGET_OS "" ) 
+
+# Add auto-a113.
+add_subdirectory( "${A113_ROOT_DIR}" "#{CMAKE_CURRENT_BINARY_DIR}/auto-a113" )
