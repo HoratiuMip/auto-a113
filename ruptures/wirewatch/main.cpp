ec34771e8649889c0f2ee10adca59c99e44449b9
mipsan <97190654+HoratiuMip@users.noreply.github.com>
2026-04-19 23:51:36 +0300

    dsa

--- ruptures/wirewatch/main.cpp
+++ ruptures/wirewatch/main.cpp
@@ -9,7 +9,16 @@
 #define WW_ASSERT_OR(c) A113_ASSERT_OR(c)
 
 struct catch_t {
+    enum type_e {
+        unknown, raw, tzsp
+    };
+    inline static const char* const type_s[] = {
+        "unknown", "raw", "tzsp"
+    };
+
     time_t   timestamp   = 0x0;
+    type_e   type        = unknown;
+    iphdr    ip_hdr      = {};
 
     struct _src_t : a113::io::ipv4_addr_pack_t { 
         a113::io::ipv4_addr_pack_t   addr;     
@@ -23,37 +32,38 @@ struct catch_t {
 std::map< std::string, catch_t >   map;
 
 void resolve_catch( catch_t ctch_ ) {
-    map[ std::format( "{}->{}", ctch_.src.addr.c_str(), ctch_.dst.addr.c_str() ) ] = std::move( ctch_ );
+    map[ std::format( "{:15} {:15}", ctch_.src.addr.c_str(), ctch_.dst.addr.c_str() ) ] = std::move( ctch_ );
 
-    std::system( "clear" );
     auto t_now = time( nullptr );
+
+    std::string out = "\n";
+
     for( auto itr = map.begin(); itr != map.end(); ) {
         auto& [ key, ctch ] = *itr;
 
         if( t_now - ctch.timestamp < 5 ) {
-            spdlog::info( "{}\t\t| {}", key, ctch.timestamp );
+            const auto* proto = getprotobynumber( ctch.ip_hdr.protocol );
+            
+            out += std::format( "{} | {:5} | {:6}\n", key, catch_t::type_s[ ctch.type ], proto ? proto->p_name : "" );
             ++itr;
         } else {
             itr = map.erase( itr );
         }
     }
+    spdlog::info( "{}", out );
 }
 
-void resolve_packet( const iphdr* ip_hdr_, time_t ts_ ) {
-    catch_t ctch = {
-        .timestamp = ts_,
-        .src = {
-            .addr = a113::io::ipv4_addr_pack_t{ ip_hdr_->saddr }
-        },
-        .dst = {
-            .addr = a113::io::ipv4_addr_pack_t{ ip_hdr_->daddr }
-        }
-    };
-
-    resolve_catch( ctch );
+void resolve_packet( catch_t&& ctch_, const iphdr* ip_hdr_ ) {
+    ctch_.src.addr = a113::io::ipv4_addr_pack_t{ ip_hdr_->saddr };
+    ctch_.dst.addr = a113::io::ipv4_addr_pack_t{ ip_hdr_->daddr };
+    resolve_catch( ctch_ );
 }
 
 void packet_handler( u_char* args_, const pcap_pkthdr* hdr_, const u_char* pck_ ) {
+    catch_t ctch = {
+        .timestamp = hdr_->ts.tv_sec
+    };
+
     const u_char* ptr  = pck_ + offsetof( ether_header, ether_type );
     const u_char* end  = pck_ + hdr_->caplen;
 
@@ -65,9 +75,22 @@ l_dive_ether_type:
             _PTR_MOVE( 2, sizeof( iphdr ) );
 
             auto* ip_hdr = ( const iphdr* )ptr;
+            
+            ctch.ip_hdr = *ip_hdr;
+
+            if( ip_hdr->protocol != IPPROTO_UDP ) {
+                ctch.type = catch_t::raw;
+                resolve_packet( std::move( ctch ), ip_hdr );
+            }
 
             _PTR_MOVE( ip_hdr->ihl * 4, sizeof( udphdr ) );
             auto* udp_hdr = ( const udphdr* )ptr;
+
+            if( ntohs( udp_hdr->uh_dport ) != 37008 ) {
+                ctch.type = catch_t::raw;
+                resolve_packet( std::move( ctch ), ip_hdr );
+                return;
+            }
             
             _PTR_MOVE( sizeof( *udp_hdr ), 0 );
             int tzsp_len = hdr_->caplen - (ptr - pck_);
@@ -89,7 +112,8 @@ l_dive_ether_type:
             }
 
             _PTR_MOVE( sizeof( ether_header ), sizeof( iphdr ) );
-            resolve_packet( ( const iphdr* )( ptr + sizeof( ether_header ) ), hdr_->ts.tv_sec );
+            ctch.type = catch_t::tzsp;
+            resolve_packet( std::move( ctch ), ( const iphdr* )( ptr + sizeof( ether_header ) ) );
         break; }
 
         case ETHERTYPE_8021Q: {
